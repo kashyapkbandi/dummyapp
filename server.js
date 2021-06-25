6fb7c20ebad82a58157ce3271cce55c752dcf137
Kashyap Kalakbandi <kkalakbandi@internal.salesforce.com>
2021-06-25 21:49:53 +0530

    dummy update

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const path = require('path');
-const hbs = require('hbs'); 
-const request = require('request');
- 
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -12,12 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/assets'));
 const viewPath = path.join(__dirname,'./templates/views');
 
-
 // tell express to use templates instead of views. 
 app.set('views',viewPath);
  
-app.get('/',(req,res)=>{
-res.render('Home');
+app.get('/home',(req,res)=>{
+res.send({"status":"200 ok"});
 });
 
 
